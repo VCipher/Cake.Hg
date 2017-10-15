1c82c382648f8a20216725e37d79d2c488cb647c
Kvachuk Grigoriy <violentcipher@gmail.com>
2017-10-15 17:23:38 +1000

    Add assemblywide cake namespace imports

--- src/Cake.Hg/Properties/AssemblyInfo.cs
+++ src/Cake.Hg/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using Cake.Core.Annotations;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -28,5 +29,11 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.3.0")]
-[assembly: AssemblyFileVersion("0.0.3.0")]
+[assembly: AssemblyVersion("0.0.4.0")]
+[assembly: AssemblyFileVersion("0.0.4.0")]
+
+// Cake build configuration
+[assembly: CakeNamespaceImport("Mercurial")]
+[assembly: CakeNamespaceImport("Cake.Hg")]
+[assembly: CakeNamespaceImport("Cake.Hg.Aliases")]
+[assembly: CakeNamespaceImport("Cake.Hg.Versions")]
