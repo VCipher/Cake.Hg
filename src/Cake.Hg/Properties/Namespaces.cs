1310186146dbad6e7cdf14fe998c4012653c7310
Kvachuk Grigoriy <violentcipher@gmail.com>
2017-10-29 20:32:32 +1000

    Add namespaces xml docs

---
+++ src/Cake.Hg/Properties/Namespaces.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+// ReSharper disable UnusedMember.Global
+
+namespace Cake.Hg.Aliases
+{
+    /// <summary>
+    /// This namespace contains Mercurial aliases.
+    /// </summary>
+    [CompilerGenerated]
+    internal class Namespaces
+    { }
+}
+
+namespace Cake.Hg.Versioning
+{
+    /// <summary>
+    /// This namespace contains extensions to Mercurial.NET for versioning projects
+    /// with semantic version standard <see cref="http://semver.org"/>.
+    /// </summary>
+    internal class NamespaceDoc
+    { }
+}
