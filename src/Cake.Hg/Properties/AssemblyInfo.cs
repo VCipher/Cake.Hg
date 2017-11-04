51dd0a35922e4a6481177ad6e61570bdd113ef6b
Kvachuk Grigoriy <violentcipher@gmail.com>
2017-11-05 01:29:06 +1000

    Separate AssemblyInfo.cs -> AssemblyInfo.cs + SolutionInfo.cs

--- src/Cake.Hg/Properties/AssemblyInfo.cs
+++ src/Cake.Hg/Properties/AssemblyInfo.cs
@@ -18,19 +18,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8E7F175A-9805-4FB2-8708-C358FF9F5CB2")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
-
 // Cake build configuration
 [assembly: CakeNamespaceImport(nameof(Mercurial))]
 [assembly: CakeNamespaceImport(nameof(Cake.Hg))]
