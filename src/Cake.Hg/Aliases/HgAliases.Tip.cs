f7ab328c4c80b9e97453058285386c3b190f3d53
Kvachuk Grigoriy <violentcipher@gmail.com>
2017-11-13 18:44:55 +1000

    Add hg tip

---
+++ src/Cake.Hg/Aliases/HgAliases.Tip.cs
@@ -0,0 +1,33 @@
+using System;
+using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.Core.IO;
+using Mercurial;
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
+
+namespace Cake.Hg.Aliases
+{
+    public static partial class HgAliases
+    {
+        /// <summary>
+        /// Get the tip revision.
+        /// </summary>
+        /// <example>
+        /// <code>
+        ///     var changeset = HgTip("./");
+        /// </code>
+        /// </example>
+        /// <param name="context">Cake context</param>
+        /// <param name="repositoryPath">Path to repository</param>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Tip")]
+        public static Changeset HgTip(this ICakeContext context, DirectoryPath repositoryPath)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            if (repositoryPath == null) throw new ArgumentNullException(nameof(repositoryPath));
+
+            return context.Hg(repositoryPath).Tip();
+        }
+    }
+}
