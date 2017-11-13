b3a00e5e0b6dac1f48e0dcaa960cecfb6a457e2c
Kvachuk Grigoriy <violentcipher@gmail.com>
2017-11-14 00:46:03 +1000

    Add tests stub for build purpose

---
+++ src/Cake.HgTests/HgAliases.CommitTests.cs
@@ -0,0 +1,15 @@
+﻿using Cake.Core;
+using NUnit.Framework;
+
+namespace Cake.HgTests
+{
+    [TestFixture]
+    public partial class HgAliasesTest : RepositoryTestsBase
+    {
+        [Test]
+        public void CommitTest()
+        { 
+            // todo: add test code
+        }
+    }
+}
