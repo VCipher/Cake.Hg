331675966c4f685b6a3be088f5d396e9e58d9873
Утин Артем Сергеевич <Utin.AS@partner.ru>
2018-02-20 11:56:26 +1000

    added alias for branch merging

--- src/Cake.Hg.Tests/HgAliasesTests.cs
+++ src/Cake.Hg.Tests/HgAliasesTests.cs
@@ -1,6 +1,8 @@
 ﻿using System.IO;
 using Cake.Hg.Tests.Fakes;
 using NUnit.Framework;
+using Mercurial;
+using System.Linq;
 
 namespace Cake.Hg.Tests
 {
@@ -31,6 +33,93 @@ namespace Cake.Hg.Tests
                 Assert.That(firstCommit, Is.Not.Empty);
                 Assert.That(diff, Is.Not.Empty);
             });
+        }
+
+        [Test]
+        public void HgMerge_ShouldMergeWithCurrentBranch()
+        {
+            var path = Repository.Path;
+            var context = new FakeCakeContext();
+            
+            context.HgInit(path);
+            var repository = context.Hg(Repository.Path);
+            
+            //default branch
+            File.WriteAllText(path + "/dummy.txt", "123");
+            context.HgCommit(path, "Initial commit");
+            var firstCommit = context.HgTip(path);
+
+            File.WriteAllText(path + "/dummy.txt", "213");
+            context.HgCommit(path, "Dummy commit");
+            var defaultCommit = context.HgTip(path);
+
+            repository.Update(firstCommit.Hash);
+
+            //dev branch
+            repository.Branch("dev");
+            File.WriteAllText(path + "/next.txt", "111");
+            context.HgCommit(path, "dev commit");
+            var devCommit = context.HgTip(path);
+
+            //back to default
+            repository.Update("default");
+
+            //merge
+            context.HgMerge(path, "dev");
+
+            //check if merge was ok
+            var mergeCommit = context.HgTip(path);
+            Assert.Multiple(() =>
+            {
+                Assert.That(mergeCommit.LeftParentHash, Is.EqualTo(defaultCommit.Hash));
+                Assert.That(mergeCommit.RightParentHash, Is.EqualTo(devCommit.Hash));
+            });
+
+        }
+
+        [Test]
+        public void HgMerge_ShouldMergeNonCurrentBranches()
+        {
+            var path = Repository.Path;
+            var context = new FakeCakeContext();
+
+            context.HgInit(path);
+            var repository = context.Hg(Repository.Path);
+
+            //default branch
+            File.WriteAllText(path + "/dummy.txt", "123");
+            context.HgCommit(path, "Initial commit");
+            var firstCommit = context.HgTip(path);
+
+            //other branch
+            repository.Branch("other");
+            File.WriteAllText(path + "/dummy.txt", "213");
+            context.HgCommit(path, "Dummy commit");
+            var otherCommit = context.HgTip(path);
+
+            repository.Update(firstCommit.Hash);
+
+            //dev branch
+            repository.Branch("dev");
+            File.WriteAllText(path + "/next.txt", "111");
+            context.HgCommit(path, "dev commit");
+            var devCommit = context.HgTip(path);
+
+            //back to default
+            repository.Update("default");
+
+            //merge
+            context.HgMerge(path, "dev", "other");
+
+            //check if merge was ok
+            var mergeCommit = context.HgTip(path);
+            Assert.Multiple(() =>
+            {
+                Assert.That(mergeCommit.Branch, Is.EqualTo("other"));
+                Assert.That(mergeCommit.LeftParentHash, Is.EqualTo(otherCommit.Hash));
+                Assert.That(mergeCommit.RightParentHash, Is.EqualTo(devCommit.Hash));
+            });
+
         }
     }
 }
---
+++ src/Cake.Hg/HgAliases.Merge.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.Core.IO;
+using Mercurial;
+
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
+
+namespace Cake.Hg
+{
+    public static partial class HgAliases
+    {
+        /// <summary>
+        /// Perform merge of two branches
+        /// </summary>
+        /// <example>
+        /// <code>
+        ///     var diff = HgMerge("./", "rc", "dev");
+        /// </code>
+        /// </example>
+        /// <param name="context">Cake context</param>
+        /// <param name="repositoryPath">Path to repository</param>
+        /// <param name="sourceBranch">Name of source branch</param>
+        /// <param name="destinationBranch">Name of destination branch (Can be ommited to merge into current branch)</param>
+        //
+        [CakeMethodAlias]
+        [CakeAliasCategory("Merge")]
+        public static void HgMerge(this ICakeContext context, DirectoryPath repositoryPath, string sourceBranch, string destinationBranch = null)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            if (repositoryPath == null) throw new ArgumentNullException(nameof(repositoryPath));
+            if (sourceBranch == null) throw new ArgumentNullException(nameof(sourceBranch));
+            
+            using (var repository = context.Hg(repositoryPath))
+            {
+                if (!string.IsNullOrEmpty(destinationBranch))
+                {
+                    repository.Update(destinationBranch);
+                }
+
+                repository.Merge(RevSpec.ByBranch(sourceBranch));
+                repository.Commit($"Merge with {sourceBranch}");
+            }
+        }
+    }
+}
